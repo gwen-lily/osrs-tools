c49d3e65524eda43aaa64bbc9035e1b9262fba92
noahgill409 <noahgill409@gmail.com>
2022-02-22 23:26:54 -0600

    a few examples
    a few hack ideas I came up with while thinking about big solos

---
+++ examples/example_rope_aggro.py
@@ -0,0 +1,224 @@
+import numpy as np
+
+from src.osrs_tools.character import *
+from src.osrs_tools import analysis_tools
+from src.osrs_tools.analysis_tools import ComparisonMode, DataMode
+
+from itertools import product
+import math
+from tabulate import tabulate
+
+
+def aggro_check(**kwargs):
+	options = {
+		'floatfmt': '.2f',
+		'scales': range(15, 32, 8)
+	}
+	options.update(kwargs)
+
+	lad = Player(name='hybrid tank')
+	lad.boost(Overload.overload())
+	lad.prayers.pray(Prayer.augury())
+
+	lad.active_style = lad.equipment.equip(
+		Gear.from_osrsbox('bearhead'),
+		Gear.from_osrsbox('imbued zamorak cape'),
+		Gear.from_osrsbox('amulet of fury'),
+		Gear.from_osrsbox('bone bolts'),
+		Weapon.from_bb('trident of the seas'),
+		Gear.from_osrsbox("Karil's leathertop"),
+		Gear.from_osrsbox('twisted buckler'),
+		Gear.from_osrsbox("Karil's leatherskirt"),
+		Gear.from_osrsbox('saradomin bracers'),
+		Gear.from_osrsbox('eternal boots'),
+		Gear.from_osrsbox('seers ring (i)'),
+	)
+
+	monster_classes = [DeathlyMage, DeathlyRanger]
+	monsters = []
+
+	for mc in monster_classes:
+		for scale in options['scales']:
+			monsters.append(mc.from_de0(scale))
+
+	indices, axes, data_ary = analysis_tools.generic_comparison_better(
+		lad,
+		target=monsters,
+		comparison_mode=ComparisonMode.PARALLEL,
+		data_mode=DataMode.POSITIVE_DAMAGE
+	)
+
+	print(data_ary)
+
+
+def hybrid_tank(**kwargs):
+	options = {
+		'floatfmt': '.1f',
+		'scales': range(15, 32, 8)
+	}
+	options.update(kwargs)
+
+	player_names = [
+		'void tank',
+		'mage tank',
+		'range tank',
+	]
+
+	lads = tuple(Player(name=name) for name in player_names)
+	void_tank, mage_tank, range_tank = lads
+
+	for lad in lads:
+		print('foo')
+		lad.active_style = lad.equipment.equip(
+			imbued_god_cape,
+			fury,
+			bone_bolts,
+			trident,
+			buckler,
+			eternals,
+			suffering
+		)
+		lad.boost(Overload.overload())
+		lad.prayers.pray(Prayer.augury())
+
+	void_tank.equipment.equip_void_set()
+
+
+def chin_estimate(**kwargs):
+	options = {
+		'floatfmt': '.1f',
+		'scales': range(15, 32, 8),
+	}
+	options.update(kwargs)
+	scales = options['scales']
+
+	greys = Weapon.from_bb('chinchompa')
+	reds = Weapon.from_bb('red chinchompa')
+	blacks = Weapon.from_bb('black chinchompa')
+
+	lad = Player(name='chinner')
+	lad.equipment.equip_basic_ranged_gear()
+	lad.equipment.equip_arma_set(zaryte=True)
+	lad.active_style = lad.equipment.equip_black_chins(buckler=True)
+	lad.boost(Overload.overload())
+	lad.prayers.pray(Prayer.rigour())
+
+	equipments = [
+		Equipment(weapon=greys),
+		Equipment(weapon=reds),
+		Equipment(weapon=blacks),
+	]
+
+	monster_classes = [DeathlyMage, DeathlyRanger]
+	monsters = [mc.from_de0(ps) for ps in scales for mc in monster_classes]
+
+	indices, axes, data_ary = analysis_tools.generic_comparison_better(
+		lad,
+		equipment=equipments,
+		target=monsters,
+		comparison_mode=ComparisonMode.CARTESIAN,
+		data_mode=DataMode.TICKS_TO_KILL,
+	)
+	slice = data_ary[0, 0, 0, 0, :, 0, :]
+
+	col_labels = axes[6][:]
+	row_labels = axes[4][:]
+	print(analysis_tools.tabulate_wrapper(slice, col_labels, row_labels, **options))
+
+
+def shaman_estimate(**kwargs):
+	options = {
+		'floatfmt': '.1f',
+		'scales': range(15, 32, 8),
+	}
+	options.update(kwargs)
+	scales = options['scales']
+
+	greys = Weapon.from_bb('chinchompa')
+	reds = Weapon.from_bb('red chinchompa')
+	blacks = Weapon.from_bb('black chinchompa')
+
+	lad = Player(name='chinner')
+	lad.equipment.equip_basic_ranged_gear()
+	lad.equipment.equip_arma_set(zaryte=True)
+	lad.equipment.equip_slayer_helm()
+	lad.active_style = lad.equipment.equip_black_chins(buckler=True)
+	lad.boost(Overload.overload())
+	lad.prayers.pray(Prayer.rigour())
+
+	equipments = [
+		Equipment(weapon=greys),
+		Equipment(weapon=reds),
+		Equipment(weapon=blacks),
+	]
+
+	monster_classes = [LizardmanShaman]
+	monsters = [mc.from_de0(ps) for ps in scales for mc in monster_classes]
+
+	indices, axes, data_ary = analysis_tools.generic_comparison_better(
+		lad,
+		equipment=equipments,
+		target=monsters,
+		comparison_mode=ComparisonMode.CARTESIAN,
+		data_mode=DataMode.TICKS_TO_KILL,
+	)
+	slice = data_ary[0, 0, 0, 0, :, 0, :]
+
+	col_labels = axes[6][:]
+	row_labels = axes[4][:]
+	print(analysis_tools.tabulate_wrapper(slice, col_labels, row_labels, **options))
+
+
+def mystic_estimates(**kwargs):
+	options = {
+		'floatfmt': '.1f',
+		'scales': range(15, 32, 8),
+	}
+	options.update(kwargs)
+	scales = options['scales']
+
+	lad = Player(name='chinner')
+	lad.equipment.equip_basic_ranged_gear(anguish=False)
+	lad.equipment.equip_salve()
+	lad.equipment.equip_arma_set(zaryte=True)
+	lad.active_style = lad.equipment.equip_black_chins(buckler=True)
+	lad.boost(Overload.overload())
+	lad.prayers.pray(Prayer.rigour())
+
+	monster_classes = [SkeletalMystic]
+	monsters = [mc.from_de0(ps) for ps in scales for mc in monster_classes]
+
+	indices, axes, data_ary = analysis_tools.generic_comparison_better(
+		lad,
+		target=monsters,
+		comparison_mode=ComparisonMode.CARTESIAN,
+		data_mode=DataMode.TICKS_TO_KILL,
+	)
+	slice = data_ary[0, 0, 0, 0, 0, 0, :]
+
+	col_labels = axes[6][:]
+	row_labels = axes[0][:]
+	print(analysis_tools.tabulate_wrapper(slice, col_labels, row_labels, **options))
+
+
+
+
+if __name__ == '__main__':
+	fury = Gear.from_osrsbox('amulet of fury')
+	bone_bolts = Gear.from_osrsbox('bone bolts')
+	trident = Weapon.from_bb('trident of the seas')
+	suffering = Gear.from_osrsbox('suffering (i)')
+	bearhead = Gear.from_osrsbox('bearhead')  # magic only from here
+	imbued_god_cape = Gear.from_osrsbox('imbued zamorak cape')
+	karils_top = Gear.from_osrsbox("Karil's leathertop")
+	buckler = Gear.from_osrsbox('twisted buckler')
+	karils_skirt = Gear.from_osrsbox("Karil's leatherskirt")
+	god_bracers = Gear.from_osrsbox('saradomin bracers')
+	eternals = Gear.from_osrsbox('eternal boots')
+
+
+	# aggro_check(scales=(31,) )
+	# chin_estimate(scales=(31,) )
+	# shaman_estimate(scales=(31,) )
+	mystic_estimates(scales=(23, 31))
+
--- examples/example_scaled_solo_mystics.py
+++ examples/example_scaled_solo_mystics.py
@@ -11,10 +11,7 @@ def room_estimates(scale: int, hammers: int = 0, thralls: bool = False, **kwargs
 	about the use of thralls and dragon warhammers so I wrote this lil method to check it out. It needs work.
 	"""
 
-	if scale >= 19:
-		mystics_count = 12
-	else:
-		raise NotImplementedError
+	mystics_count = min([3 + math.floor(scale / 3), 12])    # cap: party size 27 ~ 12 mystics
 
 	lad = Player(name='lad')
 	lad.boost(Overload.overload())
@@ -51,21 +48,90 @@ def room_estimates(scale: int, hammers: int = 0, thralls: bool = False, **kwargs
 	return table_data
 
 
-if __name__ == '__main__':
-	my_scale = 31
-	my_hammers = 3
-	using_thralls = True
+def dwh_setup(**kwargs):
+	options = {
+		'floatfmt': '.1f',
+		'datamode': DataMode.DWH_SUCCESS,
+		'scales': range(15, 32, 8),
+		'strength_levels': None
+	}
+	options.update(kwargs)
 
-	data = []
+	strength_levels = range(0, 99+1) if options['strength_levels'] is None else options['strength_levels']
 
-	for i in range(my_hammers+1):
-		row = room_estimates(my_scale, i, using_thralls)
-		row_with_label = [i] + row
-		data.append(row_with_label)
+	lad = Player(name='thwack lad')
+	levels = [PlayerLevels(attack=99, strength=i) for i in strength_levels]
 
-	headers = ['num dwh', 'kill time per mystic (ticks)', 'room completion (minutes)', 'super restores']
-	table = tabulate(data, headers=headers, floatfmt='.0f')
-	print(
-		f'{"":-^38} {my_scale:>3} scale  {"":-^38}\n',
-		table
+	boosts = [Boost.super_attack_potion()]
+	lad.prayers.pray(Prayer.piety())
+
+	lad.equipment.equip_basic_melee_gear()  # lots of overwrite
+	lad.active_style = lad.equipment.equip_dwh(tyrannical=True)
+	lad.equipment.equip_salve()
+	lad.equipment.equip(mythical_cape)
+
+	set_1 = Equipment()
+	set_1.equip_bandos_set()
+	set_1.equip(dwarven_helmet)
+	assert set_1.wearing(head=dwarven_helmet)
+	set_2 = Equipment()
+	set_2.equip_inquisitor_set()
+	set_3 = Equipment()
+	set_3.equip_void_set(elite=True)
+
+	equipments = [set_1, set_2, set_3]
+
+	mystics = [SkeletalMystic.from_de0(ps) for ps in options['scales']]
+
+	indices, axes, data_ary = analysis_tools.generic_comparison_better(
+		lad,
+		levels=levels,
+		boosts=boosts,
+		equipment=equipments,
+		target=mystics,
+		comparison_mode=ComparisonMode.CARTESIAN,
+		data_mode=options['datamode'],
+		special_attack=True,
+		**options,
 	)
+
+	slice_3d = data_ary[0, :, 0, 0, :, 0, :]
+
+	# gear_by_monster_at_0_strength = slice_3d[0, :, :]
+	gear_by_strength_at_1_scale = slice_3d[:, :, -1].T
+	column_headers: list[str | int] = [DataMode.DWH_SUCCESS.name] + [lev.strength for lev in levels]
+
+	print(analysis_tools.tabulate_wrapper(
+		gear_by_strength_at_1_scale,
+		column_headers,
+		equipments,
+		**options,
+	))
+
+
+if __name__ == '__main__':
+	# my_scale = 31
+	# my_hammers = 3
+	# using_thralls = True
+	#
+	# data = []
+	#
+	# for i in range(my_hammers+1):
+	# 	row = room_estimates(my_scale, i, using_thralls)
+	# 	row_with_label = [i] + row
+	# 	data.append(row_with_label)
+	#
+	# headers = ['num dwh', 'kill time per mystic (ticks)', 'room completion (minutes)', 'super restores']
+	# table = tabulate(data, headers=headers, floatfmt='.0f')
+	# print(
+	# 	f'{"":-^38} {my_scale:>3} scale  {"":-^38}\n',
+	# 	table
+	# )
+	dwarven_helmet = Gear.from_bb('dwarven helmet')
+	mythical_cape = Gear.from_bb('mythical cape')
+
+	float_fmts = ['5.3f', '5.0f']
+	data_modes = [DataMode.DWH_SUCCESS, DataMode.MAX_HIT]
+
+	for ff, dm in zip(float_fmts, data_modes):
+		dwh_setup(scales=(1, ), strength_levels=range(16, 32+1), floatfmt=ff, datamode=dm)
