e75928bb319f2d2496762c4cea71aa4fbb9acdba
noahgill409 <noahgill409@gmail.com>
2022-03-16 14:50:27 -0500

    analysis tools rehaul ft. bedevere_the_wise

--- src/osrs_tools/analysis_tools.py
+++ src/osrs_tools/analysis_tools.py
@@ -4,7 +4,7 @@ from itertools import product
 from tabulate import tabulate
 from typing import Iterable, Any, ParamSpec, TypeVar, Concatenate, NamedTuple
 from collections import namedtuple
-import enum
+from enum import Enum, unique
 
 from .character import *
 
@@ -13,7 +13,7 @@ R = TypeVar('R')
 datamode_type = namedtuple('datamode', ['key', 'dtype', 'attribute'])
 
 
-class DataMode(enum.Enum):
+class DataMode(Enum):
 	DPS = datamode_type('damage per second', float, 'per_second')
 	DAMAGE_PER_SECOND = DPS
 	DPT = datamode_type('damage per tick', float, 'per_tick')
@@ -36,9 +36,19 @@ class DataMode(enum.Enum):
 	HOURS_TO_KILL = datamode_type('hours to kill', float, None)
 
 
-class ComparisonMode(enum.Enum):
-	PARALLEL = 'parallel'       # (A | B) -> (A1, B1), (A2, B2)
-	CARTESIAN = 'cartesian'     # (A x B) -> (A1, B1), (A1, B2), (A2, B1), (A2, B2)
+@unique
+class DataAxes(Enum):
+	players = 'players'
+	targets = 'targets'
+	levels = 'levels'
+	prayers = 'prayers'
+	boosts = 'boosts'
+	equipment = 'equipment'
+	active_style = 'active_style'
+
+# class ComparisonMode(enum.Enum):
+# 	PARALLEL = 'parallel'       # (A | B) -> (A1, B1), (A2, B2)
+# 	CARTESIAN = 'cartesian'     # (A x B) -> (A1, B1), (A1, B2), (A2, B1), (A2, B2)
 
 
 def tabulate_wrapper(data, col_labels: list, row_labels: list, meta_header: str = None, **kwargs):
@@ -79,341 +89,177 @@ def tabulate_wrapper(data, col_labels: list, row_labels: list, meta_header: str 
 	return table
 
 
-def graph_wrapper(func: Callable[P, R]) -> Callable[P, R]:
-
-	@wraps(func)
-	def inner(*args: P.args, **kwargs: P.kwargs) -> R:
-		input_ary, data_ary = func(*args, **kwargs)
-		options = {
-			'data': 'dps',
-		}
-		options.update(kwargs)
-		m, n = data_ary.shape
-
-		for i in range(m):
-			y = data_ary[i, :]
-			plt.plot(input_ary, y)
-
-		plt.title(options['data'])
-		plt.xlabel('scale')
-		plt.ylabel(options['data'])
-		plt.legend([p.name for p in options['players']])
-		plt.show()
-
-		return input_ary, data_ary
-
-	return inner
-
-
-def table_wrapper(func: Callable[P, R]) -> Callable[P, R]:
-
-	@wraps(func)
-	def wrapper_func(*args: P.args, **kwargs: P.kwargs) -> R:
-		input_ary, data_ary = func(*args, **kwargs)
-		options = {
-			'headers': None,
-			'floatfmt': '.1f',
-			'tablefmt': 'simple'
-		}
-		options.update(kwargs)
-
-		data = []
-		for i, player in enumerate(options['players']):
-			row = [player.name]
-			row.extend(list(data_ary[i, :]))
-			data.append(row)
-
-		if headers := options['headers']:
-			pass
-		else:
-			headers = [options['data']]
-			headers.extend([str(x) for x in input_ary])
-
-		table = tabulate(data, headers=headers, floatfmt=options['floatfmt'], tablefmt=options['tablefmt'])
-		print(table)
-
-		return input_ary, data_ary
-
-	return wrapper_func
-
-
-def scale_comparison(*, players: tuple[Player, ...], monster_name: str, **kwargs):
-	options = {
-		'scales': range(1, 100+1),
-		'data': 'object'
-	}
-	options.update(kwargs)
-	scales_ary = np.asarray(options['scales'])
-	d_ary = np.empty(shape=(len(players), scales_ary.size), dtype=object)
-
-	for j, scale in enumerate(scales_ary):
-		monster = CoxMonster.from_de0(monster_name, scale)
-
-		for i, player in enumerate(players):
-			dam_ij = player.damage_distribution(monster)
-
-			if options['data'] == 'object':
-				d_ary[i, j] = dam_ij
-			elif options['data'] == 'dps':
-				d_ary[i, j] = dam_ij.per_second
-			elif options['data'] == 'dpt':
-				d_ary[i, j] = dam_ij.per_tick
-			elif options['data'] == 'mean':
-				d_ary[i, j] = dam_ij.mean
-			elif options['data'] == 'max hit':
-				d_ary[i, j] = dam_ij.max_hit
-			else:
-				raise NotImplementedError
-
-	return scales_ary, d_ary
-
-
-def variable_comparison(*, players: tuple[Player, ...], monster_name: str, skill_var: str, values: Iterable, **kwargs):
-	options = {
-		'data': 'object',
-	}
-	options.update(kwargs)
-	values_ary = np.asarray(values)
-
-	d_ary = np.empty(shape=(len(players), values_ary.size))
-	monster = CoxMonster.from_de0(monster_name, 100)
-
-	for j, value in enumerate(values):
-		monster.active_levels.__setattr__(skill_var, value)
-
-		for i, player in enumerate(players):
-			dam_ij = player.damage_distribution(monster)
-
-			if options['data'] == 'object':
-				d_ary[i, j] = dam_ij
-			elif options['data'] == 'dps':
-				d_ary[i, j] = dam_ij.per_second
-			elif options['data'] == 'dpt':
-				d_ary[i, j] = dam_ij.per_tick
-			elif options['data'] == 'mean':
-				d_ary[i, j] = dam_ij.mean
-			elif options['data'] == 'max hit':
-				d_ary[i, j] = dam_ij.max_hit
-			else:
-				raise NotImplementedError
-
-	return values_ary, d_ary
-
-
-def equipment_comparison(
-		*,
-		players: tuple[Player, ...],
-		equipments: tuple[Equipment, ...],
-		data_func: Callable[P, R],
-		target: Character | None,
-		**kwargs
-):
-	"""
-
-	:param players: A tuple containing Players.
-	:param equipments: A tuple containing Equipments.
-	:param data_func: A callable function which returns data. The first argument must accept Player. The second must
-		accept Equipment. The third may accept Character or not exist.
-	:param target: The Character
-	:param kwargs:
-	:return:
-	"""
-	if target:
-		inputs = tuple(zip(equipments, (target, )*len(equipments)))
-	else:
-		inputs = equipments
-
-	return generic_comparison(
-		players=players,
-		data_func=data_func,
-		input_vals=inputs,
-		**kwargs
-	)
-
-
-def generic_damage_method(
-		player: Player,
-		levels: PlayerLevels | None,
-		prayers: PrayerCollection | None,
-		boosts: BoostCollection | None,
-		equipment: Equipment | None,
-		active_style: PlayerStyle | None,
-		target: Character,
-		*args,
-		**kwargs
+def generic_player_damage_distribution(
+	player: Player,
+	target: Character,
+    levels: PlayerLevels = None,
+    prayers: PrayerCollection = None,
+    boost: Boost = None,
+    equipment: Equipment = None,
+    active_style: PlayerStyle = None,
+    **kwargs
 ) -> Damage:
+	"""Return a damage distribution from player & target, with optional modifiers.
+
+	See the bedevere_the_wise method for more info on each of the parameters.
+
+	Args:
+		player (Player): A Player, a copy of which is modified within the method.
+		target (Character): The target.
+		levels (PlayerLevels, optional): Optional substitute for player.levels. Defaults to None.
+		prayers (PrayerCollection, optional): Optional substitute for player.prayers_coll. Defaults to None.
+		boost (Boost, optional): Optional substitute for active boosts, will reset all boosts per application. Defaults to None.
+		equipment (Equipment, optional): Optional equipment to add, can include weapons with default styles. Defaults to None.
+		active_style (PlayerStyle, optional): Optional style to specify non-gear-based style-swaps. Defaults to None.
+
+	Returns:
+		Damage: _description_
+	"""
+	play = copy(player) 	# make a copy of player
+
 	if levels is not None:
-		player.levels = levels
-		player.reset_stats()
+		play.base_levels = levels
+		play.reset_stats()
 
 	if prayers is not None:
-		player.prayers = prayers
+		play.prayers_coll = prayers
 
-	if boosts is not None:
-		player.reset_stats()
-		player.boost(boosts)
+	if boost is not None:
+		play.reset_stats()
+		play.boost(boost)
 
-	if equip_reset := (equipment is not None):
-		base_equip = copy(player.equipment)
-		base_style = player.active_style
+	if equip_reset := (equipment is not None):		
+		if active_style is not None:
+			play.active_style = play.equipment.equip(equipment.equipped_gear, style=active_style)
+		else:
+			return_style = play.equipment.equip(equipment.equipped_gear)
+			if return_style is not None:
+				play.active_style = return_style
 
-		return_style = player.equipment.equip(*astuple(equipment, recurse=False), style=active_style)
+	elif style_changed := (active_style is not None):
+		play.active_style = active_style
 
-		if return_style is not None:
-			player.active_style = return_style
-
-	else:
-		base_equip, base_style = (None, )*2
-
-	# not redundant, style may change without gear changing.
-	if active_style is not None:
-		player.active_style = active_style
-
-	dam = player.damage_distribution(other=target, *args, **kwargs)
-
-	# cleanup
-	if equip_reset:
-		player.equipment.equip(*astuple(base_equip, recurse=False))
-		player.active_style = base_style
-
-	return dam
+	return play.damage_distribution(other=target, **kwargs)
 
 
-def generic_comparison_better(
-		players: Player | list[Player],
-		*,
-		levels: PlayerLevels | list[PlayerLevels] = None,
-		prayers: Prayer | PrayerCollection | list[Prayer | PrayerCollection] = None,
-		boosts: Boost | BoostCollection | list[Boost | BoostCollection] = None,
-		equipment: Equipment | list[Equipment] = None,
-		active_style: PlayerStyle | list[PlayerStyle] = None,
-		target: Character | list[Character] = None,
-		comparison_mode: ComparisonMode.PARALLEL,
-		data_mode: DataMode = DataMode.DPS,
-		**kwargs,
-) -> tuple[tuple[int], list[list], np.ndarray]:
+def bedevere_the_wise(
+	players: Player | list[Player],
+	targets: Character | list[Character],
+	*,
+	levels: PlayerLevels | list[PlayerLevels] = None,
+	prayers: PrayerCollection | list[PrayerCollection] = None,
+	boosts: Boost | list[Boost] = None,
+	equipment: Equipment | list[Equipment] = None,
+	active_style: PlayerStyle | list[PlayerStyle] = None,
+	data_mode: DataMode = DataMode.DPT,
+	**kwargs,
+) -> tuple[dict[DataAxes, list], tuple[int], np.ndarray]:
+	"""Smart comparison interface for a generic player damage method.
+
+	Args:																		||
+		players (Player | list[Player]): Player objects, which all optional 
+			parameters may modify by some method.
+		targets (Character | list[Character]): Character objects against which to
+			calculate the players' damage potential.
+		levels (PlayerLevels | list[PlayerLevels], optional): PlayerLevels 
+			objects which can modify the base_levels of players. May raise an 
+			EquippableError if the provided levels are beneath that of equipment 
+			requirements. Defaults to None.
+		prayers (PrayerCollection | list[PrayerCollection], optional): 
+			PrayerCollection objects to be prayed by the players. Defaults to None.
+		boosts (Boost | list[Boost], optional): Boosts objects to boost the 
+			players. Defaults to None.
+		equipment (Equipment | list[Equipment], optional): Equipment objects to 
+			be equipped by the players. If the active_style parameter is specified, 
+			it will override any default styles suggested by the equipment provided.
+			Defaults to None.
+		active_style (PlayerStyle | list[PlayerStyle] | None, optional): Styles 
+			objects, will raise StyleErrors if the equipment provided does not have
+			such styles. Defaults to None.
+		data_mode (DataMode, optional): The data protocol to follow. See 
+			DataModes for more information. Defaults to DataMode.DPT.
+		**kwargs: 
+			special_attack: bool, 
+			distance: int, 
+			spell: Spell, 
+			additional_targets: int | Character | list[Character], 
+		**additional_kwargs:
+
+	Raises:
+		NotImplementedError: Raised with unrecognized DataMode inputs.
+		StyleError: Raised if the equipment provided does not have such styles.
+		EquippableError: Raised if there is conflict between levels and
+		equipment requirements.
+
+
+	Returns:
+		axes_dict (dict[DataAxes, tuple]): A dictionary for looking up axes indices. 
+		indices (tuple[int]): An iterable list of all indices of the data array.
+		data_ary (np.ndarray): The data array.
+	"""
+	# prepare all single instances as lists
 	if isinstance(players, Player):
 		players = [players]
 
-	if levels is None:
-		levels = [None]
-	elif isinstance(levels, PlayerLevels):
+	if isinstance(targets, Character):
+		targets = [targets]
+
+	if isinstance(levels, PlayerLevels):
 		levels = [levels]
 
-	if prayers is None:
-		prayers = [None]
-	elif isinstance(prayers, Prayer):
-		prayers = [PrayerCollection(prayers=(prayers, ))]
-	elif isinstance(prayers, PrayerCollection):
+	if isinstance(prayers, PrayerCollection):
 		prayers = [prayers]
-	elif isinstance(prayers, list):
-		for index, e in enumerate(prayers[:]):
-			if isinstance(e, Prayer):
-				prayers[index] = PrayerCollection(prayers=(e, ))
 
-	if boosts is None:
-		boosts = [None]
-	elif isinstance(boosts, Boost):
-		boosts = [BoostCollection(boosts=(boosts, ))]
-	elif isinstance(boosts, BoostCollection):
+	if isinstance(boosts, Boost):
 		boosts = [boosts]
-	elif isinstance(boosts, list):
-		for index, e in enumerate(boosts[:]):
-			if isinstance(e, Boost):
-				boosts[index] = BoostCollection(boosts=(e, ))
 
-	if equipment is None:
-		equipment = [None]
-	elif isinstance(equipment, Equipment):
+	if isinstance(equipment, Equipment):
 		equipment = [equipment]
 
-	if active_style is None:
-		active_style = [None]
-	elif isinstance(active_style, PlayerStyle):
+	if isinstance(active_style, PlayerStyle):
 		active_style = [active_style]
 
-	if target is None:
-		target = [None]
-	elif isinstance(target, Character):
-		target = [target]
-
-	axes = [players, levels, prayers, boosts, equipment, active_style, target]
+	# prepare kw and tuple axes, eliminate None values.
+	axes_dict = {
+		DataAxes.players: players,
+		DataAxes.targets: targets,
+		DataAxes.levels: levels,
+		DataAxes.prayers: prayers,
+		DataAxes.boosts: boosts,
+		DataAxes.equipment: equipment,
+		DataAxes.active_style: active_style
+	}
+	axes_dict = {k: v for k, v in axes_dict.items() if v is not None}
+	axes = tuple(axes_dict.values())
 	axes_dims = tuple(len(ax) for ax in axes)
+	# create an axes_index_dict for use within the loop
+	axes_index_dict = {k: axes.index(v) for k, v in axes_dict.items()}
 
-	if comparison_mode == ComparisonMode.PARALLEL:
-		max_dim = max(axes_dims)
-		indices = tuple(range(max_dim))
-		data_ary = np.empty(shape=(max_dim, ), dtype=data_mode.value.dtype)
+	# get generated indices and a complete parameter space with itertools.product
+	indices = tuple(product(*(range(n) for n in axes_dims)))
+	params_cart_prod = product(*axes) 	# cartesian product of all parameter axes
+	data_ary = np.empty(shape=axes_dims, dtype=data_mode.value.dtype)
 
-		for index, axis in enumerate(axes[:]):
-			if len(axis) == 1:
-				axes[index] = axis * max_dim
+	for index, params in zip(indices, params_cart_prod, strict=True):
+		dam = generic_player_damage_distribution(*params, **kwargs)
 
-		for index, params in enumerate(zip(*axes, strict=True)):
-			dam = generic_damage_method(*params, **kwargs)
+		if data_mode.value.attribute is not None:
 			data_ary[index] = dam.__getattribute__(data_mode.value.attribute)
+		else:
+			target: Character = params[axes_index_dict[DataAxes.targets]]
+			hp = target.levels.hitpoints
 
-	elif comparison_mode == ComparisonMode.CARTESIAN:
-		indices = tuple(product(*(range(n) for n in axes_dims)))
-		cartesian_product = product(*axes)
-		data_ary = np.empty(shape=axes_dims, dtype=data_mode.value.dtype)
-
-		for index, params in zip(indices, cartesian_product):
-			dam = generic_damage_method(*params, **kwargs)
-
-			if data_mode.value.attribute is not None:
-				data_ary[index] = dam.__getattribute__(data_mode.value.attribute)
+			if data_mode is DataMode.TICKS_TO_KILL:
+				value = hp / dam.per_tick
+			elif data_mode is DataMode.SECONDS_TO_KILL:
+				value = hp / dam.per_second
+			elif data_mode is DataMode.MINUTES_TO_KILL:
+				value = hp / dam.per_minute
+			elif data_mode is DataMode.HOURS_TO_KILL:
+				value = hp / dam.per_hour
 			else:
-				target = params[-1]
-				hp = target.active_levels.hitpoints
+				raise NotImplementedError
 
-				if data_mode is DataMode.TICKS_TO_KILL:
-					data_ary[index] = hp / dam.per_tick
-				elif data_mode is DataMode.SECONDS_TO_KILL:
-					data_ary[index] = hp / dam.per_second
-				elif data_mode is DataMode.MINUTES_TO_KILL:
-					data_ary[index] = hp / dam.per_minute
-				elif data_mode is DataMode.HOURS_TO_KILL:
-					data_ary[index] = hp / dam.per_hour
-				else:
-					raise NotImplementedError
+			data_ary[index] = value
 
-	else:
-		raise NotImplementedError
-
-	return indices, axes, data_ary
-
-
-
-def generic_comparison(
-		*,
-		players: list[Player],
-		data_func: Callable[P, R],
-		input_vals: Iterable,
-		**kwargs
-):
-	"""
-
-	:param players: A tuple containing Players.
-	:param data_func: A callable function which returns data. The first argument must accept Player.
-	:param input_vals: An Iterable whose elements are passed to data_func. If different Players require different params
-		input_vals must be an Iterable of Iterables.
-	:param kwargs:
-	:return:
-	"""
-	input_ary = np.asarray([str(v) for v in input_vals], dtype=str)
-	data_ary = np.empty(shape=(len(players), input_ary.size))
-
-	for i, player in enumerate(players):
-		for j, input_j in enumerate(input_vals):
-			try:
-				data_ary[i, j] = data_func(player, *input_j, **kwargs)
-			except TypeError:
-				data_ary[i, j] = data_func(player, input_j, **kwargs)
-
-	return input_ary, data_ary
-
-
-
-
-
+	return indices, axes_dict, data_ary
